0f1ecfe6265176d995481a889eeb1166b38bf4ed
muhamood <nmuhamood3@gmail.com>
2021-03-16 14:50:43 +0300

    creating todo endpoint

--- backend/todo/controller.js
+++ backend/todo/controller.js
@@ -1,6 +1,17 @@
+const Todo = require('../model/todo');
 
 const getTodo = (req, res) =>{
   return res.send("Todo successfully retrieved");
 }
 
-module.exports = { getTodo };
+const createTodo = async (req, res) =>{
+  const newTodo = new Todo(req.body);
+  await newTodo.save();
+  return res.send({
+    success: true,
+    message: "Todo created successfully",
+    newTodo
+  });
+}
+
+module.exports = { getTodo, createTodo };
