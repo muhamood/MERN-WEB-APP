3c76c72e67eeea9443baf59766c91ff3002abcf0
muhamood <nmuhamood3@gmail.com>
2021-03-16 13:04:31 +0300

    installing and setting up body-parser package

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const todoRouter = require('./todo');
 const PORT = process.env.PORT || 6000;
 const db = require('./database');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.use('/', todoRouter);
 
