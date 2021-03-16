438fb846ba7d9f0dc2c9d916fb265020d7ba85ed
muhamood <nmuhamood3@gmail.com>
2021-03-16 11:10:31 +0300

    setup the server port and welcome message displayed when server running

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>{
+   return res.send('Hello world, Welcome to MERN web App');
+});
+
+app.listen(PORT, () => console.log(`Serving running on port ${PORT}`));
