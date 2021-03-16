f140941263b9328c75ea5863f2efbffb9586ece9
muhamood <nmuhamood3@gmail.com>
2021-03-16 12:48:29 +0300

    importing todo Route to the server file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 
 const express = require('express');
 const app = express();
+const todoRouter = require('./todo');
 const PORT = process.env.PORT || 6000;
 const db = require('./database');
 
 app.get('/', (req, res) =>{
    return res.send('Hello world, Welcome to MERN web App');
 });
+
+app.use('/', todoRouter);
 
 db().then(console.log('Database started successfully'))
     .catch(err => console.log(err));
